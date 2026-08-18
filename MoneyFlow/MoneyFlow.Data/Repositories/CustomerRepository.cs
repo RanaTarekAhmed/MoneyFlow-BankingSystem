64ae721c07019b12633cbcefe526892f34c74d8a
EslamM0hamady <eslammohamady39@gmail.com>
2026-08-18 19:50:01 +0300

    feat: add customer repository

---
+++ MoneyFlow/MoneyFlow.Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyFlow.Data.Database;
+using MoneyFlow.Data.Entities;
+using MoneyFlow.Data.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+
+namespace MoneyFlow.Data.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly MoneyFlowDbContext _context;
+
+        public CustomerRepository(MoneyFlowDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Customer customer)
+        {
+            await _context.Customers.AddAsync(customer);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(c => !c.IsDeleted && c.Id == id);
+
+            if (customer == null)
+            {
+                return;
+            }
+
+            customer.Delete();
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Customer?> GetAsync(Expression<Func<Customer, bool>>? filter)
+        {
+            var query = _context.Customers.Where(c => !c.IsDeleted);
+
+            if (filter != null)
+            {
+                return await query.FirstOrDefaultAsync(filter);
+            }
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Customer>> GetAllAsync(Expression<Func<Customer, bool>>? filter)
+        {
+            var query = _context.Customers.Where(c => !c.IsDeleted);
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            return await query.ToListAsync();
+        }
+    }
+}
---
+++ MoneyFlow/MoneyFlow.Data/Repositories/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using MoneyFlow.Data.Entities;
+using System.Linq.Expressions;
+
+
+namespace MoneyFlow.Data.Repositories.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<List<Customer>> GetAllAsync(Expression<Func<Customer, bool>>? filter);
+        Task<Customer?> GetAsync(Expression<Func<Customer, bool>>? filter);
+        Task AddAsync(Customer customer);
+        Task DeleteAsync(int id);
+    }
+}
