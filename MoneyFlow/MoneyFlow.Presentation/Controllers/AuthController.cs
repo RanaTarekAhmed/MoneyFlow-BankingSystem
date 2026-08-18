2c35c6aee996251170750f447caf8ec709033a56
EslamM0hamady <eslammohamady39@gmail.com>
2026-08-19 02:03:31 +0300

    feat: implement home and authentication UI

---
+++ MoneyFlow/MoneyFlow.Presentation/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyFlowSandbox.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
