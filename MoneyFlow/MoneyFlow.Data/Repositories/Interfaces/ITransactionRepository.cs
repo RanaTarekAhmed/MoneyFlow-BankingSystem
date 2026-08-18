5eb99045604ff092f819de532006519675ca6695
HagerrIbrahim <hageribrahim808@gmail.com>
2026-08-18 23:56:26 +0300

    Implement transaction repository

---
+++ MoneyFlow/MoneyFlow.Data/Repositories/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using MoneyFlow.Data.Entities;
+using MoneyFlow.Data.Enums;
+
+
+namespace MoneyFlow.Data.Repositories.Interfaces
+{
+    public interface ITransactionRepository
+    {
+        Task<List<Transaction>> GetAllAsync(Expression<Func<Transaction, bool>>? filter);
+        Task<Transaction?> GetAsync(Expression<Func<Transaction, bool>> filter);
+        Task AddAsync(Transaction transaction);
+        Task UpdateStatusAsync(int id, TransactionStatus status);
+    }
+}
---
+++ MoneyFlow/MoneyFlow.Data/Repositories/TransactionRepository.cs
@@ -0,0 +1,59 @@
+﻿using MoneyFlow.Data.Entities;
+using MoneyFlow.Data.Enums;
+using MoneyFlow.Data.Repositories.Interfaces;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using MoneyFlow.Data.Database;
+
+
+
+namespace MoneyFlow.Data.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly MoneyFlowDbContext dp_context;
+
+        public TransactionRepository(MoneyFlowDbContext DP)
+        {
+            dp_context = DP;
+        }
+        public async Task AddAsync(Transaction transaction)
+        {
+
+            await dp_context.Transactions.AddAsync(transaction);
+            await dp_context.SaveChangesAsync();
+
+        }
+
+        public async Task<List<Transaction>> GetAllAsync(Expression<Func<Transaction, bool>>? filter)
+        {
+
+            if (filter != null)
+            {
+                return await dp_context.Transactions.Where(filter).ToListAsync();
+            }
+
+            return await dp_context.Transactions.ToListAsync();
+
+        }
+
+        public async Task<Transaction?> GetAsync(Expression<Func<Transaction, bool>> filter)
+        {
+            return await dp_context.Transactions.Where(filter).FirstOrDefaultAsync();
+
+        }
+
+        public async Task UpdateStatusAsync(int id, TransactionStatus status)
+        {
+            var transaction = await dp_context.Transactions.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (transaction != null)
+            {
+                transaction.UpdateStatus(status);
+
+                await dp_context.SaveChangesAsync();
+            }
+
+        }
+    }
+}
