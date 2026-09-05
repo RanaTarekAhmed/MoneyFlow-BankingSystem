968b6252df542d2cad38658bdc83092d977dc8a6
HagerrIbrahim <hageribrahim808@gmail.com>
2026-09-05 06:05:48 +0300

    removing transactions from myaccounts page

--- MoneyFlow/MoneyFlow.Business/ViewModels/Accounts/MyAccountsVM.cs
+++ MoneyFlow/MoneyFlow.Business/ViewModels/Accounts/MyAccountsVM.cs
@@ -3,6 +3,6 @@
     public class MyAccountsVM
     {
         public List<AccountSummaryVM> Accounts { get; set; } = new();
-        public List<TransactionVM> Transactions { get; set; } = new();
+        
     }
 }
